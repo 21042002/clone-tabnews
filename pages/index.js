0e0c69ea14839d28eb1ced9b0f58dd21d32b2b73
tamireslima <tamireslima.tb@gmail.com>
2025-11-04 23:20:40 -0300

    adciona arquivo   e pages/index.js houve uma atualização do titulo

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ function Home() {
     return <h1>Olá Pessoal, vocês estão bem? Se estiver, responda com joinha!! </h1>
 }
 
+// Colocando como padrão para importar o que for escrito
 // Exportar função
 export default Home;
 
