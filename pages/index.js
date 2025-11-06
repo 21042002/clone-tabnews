f38f8a73958a391377364a60e8cfa75873b3344f
tamireslima <tamireslima.tb@gmail.com>
2025-11-06 20:15:50 -0300

    adiciona scripts e

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 function Home() {
-    return <h1>Olá Pessoal, vocês estão bem? Se estiver, responda com joinha!! </h1>
+  return (
+    <h1>Olá Pessoal, vocês estão bem? Se estiver, responda com joinha!! </h1>
+  );
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 // Colocando como padrão para importar o que for escrito
 // Exportar função
 export default Home;
-
